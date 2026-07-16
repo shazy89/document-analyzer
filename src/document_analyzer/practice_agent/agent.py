2bc439d5c2ef032dd2a9d3313b157f7a7a96546d
Erdoan Shaziman <eshaziman@finpro.us>
2026-07-16 14:28:14 -0400

    initial commit

--- src/document_analyzer/practice_agent/agent.py
+++ src/document_analyzer/practice_agent/agent.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-from typing import TypedDict
-
+from typing import Literal, TypedDict
+from datetime import datetime 
+from pydantic import BaseModel, Field
 from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage, ToolMessage
 from typing_extensions import Annotated
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, END, START
 from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
-
+from langgraph.prebuilt import ToolNode
 from document_analyzer.analyzer_agent.config import DocumentAnalyzerConfig
 from langgraph.checkpoint.memory import MemorySaver
 memory = MemorySaver()
@@ -15,145 +16,97 @@ memory = MemorySaver()
 from dotenv import load_dotenv
 load_dotenv()
 
+spending_category = [{
+    "date": "2026-05-25",
+    "description": "PS&G",
+    "amount": 120.42,
+    "category": "Utilities",
+    "type": "expense"
+  },
+    {
+    "date": "2026-06-25",
+      "description": "PS&G",
+    "amount": 320.42,
+    "category": "Utilities",
+    "type": "expense"
+  },
+    {
+    "date": "2026-07-25",
+    "description": "PS&G",
+    "amount": 300.42,
+    "category": "Utilities",
+    "type": "expense"
+  },
+    {
+    "date": "2026-08-25",
+    "description": "PS&G",
+    "amount": 140.42,
+    "category": "Utilities",
+    "type": "expense"
+  },
+    {
+    "date": "2026-09-25",
+    "description": "PS&G",
+    "amount": 120.42,
+    "category": "Utilities",
+    "type": "expense"
+  }
+]
+class DataRequest(BaseModel):
+    intent: Literal[
+        "spending_by_category",
+        "total_spending"
+    ]
+    period: datetime | None = None
+    category: str | None = None
+
+class UIBlock(BaseModel):
+    component: Literal[
+        "summary_cards",
+        "bar_chart",
+        "transactions_table",
+    ]
+    title: str
+    data_request: DataRequest
+
+class UIPlan(BaseModel):
+    message: str
+    blocks: list[UIBlock] = Field(min_length=1, max_length=3)
 
 @tool
-def capitalize_first_letter(s: str) -> str:
-    """Capitalize the first letter of a string."""
-    if not s:
-        return s
-    return s[0].upper() + s[1:]
-
-@tool
-def multiply(a: int, b: int) -> int:
-    """Multiply `a` and `b`.
-
-    Args:
-        a: First int
-        b: Second int
+def spending_by_category():
     """
-    return a * b
+    Tool to get spending by category.
+    """
+    return    spending_category
 
 @tool
-def add(a: int, b: int) -> int:
-    """Adds `a` and `b`.
-
-    Args:
-        a: First int
-        b: Second int
+def total_spending():
     """
-    return a + b
-
-
-@tool
-def divide(a: int, b: int) -> float:
-    """Divide `a` and `b`.
-
-    Args:
-        a: First int
-        b: Second int
+    Tool to get total spending.
     """
-    return a / b
+    total = sum(item["amount"] for item in spending_category)
+    return total
 
-tools = [add, multiply, divide]
+ 
 
-tools_by_name = {tool.name: tool for tool in tools}
 
-class MessagesState(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages]
-    llm_calls: int
-
-class PracticeAgent:
-    def __init__(self, config: DocumentAnalyzerConfig, checkpointer: MemorySaver) -> None:
-        self.system = """
-You are a helpful assistant that performs arithmetic operations.
-
-Rules:
-- If a calculation is needed, call the correct tool.
-- After receiving the tool result, answer in plain English.
-- Do not prefix your answer with words like 'assistant' or 'final'.
-"""
+class UiAgent:
+    def __init__(self, config: DocumentAnalyzerConfig):
         self.config = config
-        self.checkpointer = checkpointer
-        
-        self._llm = self._llm_model()
-        
-        
-    def _llm_model(self):
-        return ChatOpenAI(model=self.config.model_name, temperature=0, api_key=self.config.api_key, base_url=self.config.api_base).bind_tools(tools)    
+        self.llm = ChatOpenAI(
+            model_name=config.model_name,
+            temperature=config.temperature,
+            api_key=config.api_key,
+            base_url=config.api_base
+        )
+
+    def builder_graph(self):
+        builder = StateGraph(UIPlan)
+
+        builder.add_node("tools", ToolNode([spending_by_category, total_spending]))    
+
+        return builder.compile()
     
-    def llm_call(self, state: MessagesState) -> MessagesState:
-        messages = state.get("messages", [])
-        llm_calls = state.get("llm_calls", 0)
-        
-        system_prompt = SystemMessage(
-                        content=self.system
-                    )
-        
-        messages = [system_prompt] + messages
-        content = self._llm.invoke(messages)
-        
-        return {"messages": [content], "llm_calls": llm_calls + 1}
-    
-    def take_action(self, state: MessagesState) -> MessagesState:
-        last_message = state["messages"][-1]
-        tool_calls = getattr(last_message, "tool_calls", [])
-        llm_calls = state.get("llm_calls", 0) + 1
 
     
-        result = []
-        if not tool_calls:
-            return {"messages": [], "llm_calls": llm_calls}
-        
-        for t in tool_calls:
-            tool = tools_by_name[t["name"]]
-            observation = tool.invoke(t.get("args", {}))
-            result.append(
-                ToolMessage(
-                    content=str(observation),
-                    name=t["name"],
-                    tool_call_id=t["id"],
-                )
-            )
-        
-        return {"messages": result, "llm_calls": llm_calls}
-    
-    def should_continue(self, state: MessagesState) -> str:
-        last_message = state["messages"][-1]
-        if getattr(last_message, "tool_calls", []):
-            return "action"
-        return END
-
-    def build_graph(self):
-        graph = StateGraph(MessagesState)
-        
-        graph.add_node("llm", self.llm_call)
-        graph.add_node("action", self.take_action)
-        
-        graph.add_conditional_edges("llm", self.should_continue)
-        graph.add_edge("action", "llm")
-        graph.set_entry_point("llm")
-        
-        return graph.compile(checkpointer=self.checkpointer)
-        
-        
-def main() -> None:
-    config = DocumentAnalyzerConfig.from_env()
-    agent = PracticeAgent(config=config, checkpointer=memory)
-    graph = agent.build_graph()
-
-    thread4 = {"configurable": {"thread_id": "1234"}}
-    messages = [HumanMessage(content="What is 5 multiplied by 3?")]
-
-    for event in graph.stream({"messages": messages, "llm_calls": 0}, thread4):
-        print(f"Event: {event}")
-
-        if "llm" in event:
-            ai_message = event["llm"]["messages"][-1]
-
-            # Only log final answer when there are no tool calls.
-            if not getattr(ai_message, "tool_calls", []):
-                print("Final answer:", ai_message.content)
-
-
-if __name__ == "__main__":
-    main()
